c00363c2a81d5b6592c63f565b6fb441c539767a
levicarlosz <olevicarlos@gmail.com>
2023-03-05 20:55:01 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid",
+    "./snippets/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
